6b04b8158f6a8a46e2df26f1fabf9eed70d0bd18
Cameron Johnson <camkjohnson64@gmail.com>
2020-03-25 20:50:50 -0400

    latest version with new state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import AddEvent from "./Components/AddEvent/AddEvent";
+import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+// import "./App.css";
+// import AddEvent from "./Components/AddEvent/AddEvent";
+import CurrentLocation from "./Components/CurrentLocation/CurrentLocation";
+
+let image;
+
+let marker;
 
 class App extends Component {
   state = {
-    events: [{ id: "", name: "", eventName: "" }]
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
+    markers: [
+      {
+        position: { lat: 34.0388319, lng: -84.5827453 },
+        info: { name: "Alpha Delta Nu Covd 19 Giftcard giveaway" }
+      },
+      {
+        position: { lat: 33.7476109, lng: -84.3945527 },
+        info: { name: "Alpha Delta Nu Covd 19 Giftcard giveaway" }
+      },
+      {
+        position: { lat: 33.78157424926758, lng: -84.41114807128906 },
+        info: { name: "Alpha Delta Nu Covd 19 Giftcard giveaway" }
+      }
+
+]
+
+}
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   };
+
+  image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+  marker = (
+    <div>
+      {this.state.markers.map((data, i) => {
+        console.log(data);
+        return (
+          <Marker
+            position={data.position}
+            name={data.info.name}
+            onClick={this.onMarkerClick}
+          />
+        );
+      })}
+    </div>
+  );
 
   // changeHandler = (event) => {};
 
@@ -22,21 +79,44 @@ class App extends Component {
         <div className="jumbotron">
           <h1> Welcome to Helping Hand</h1>
         </div>
+        <div>
+          <CurrentLocation
+            centerAroundCurrentLocation
+            google={this.props.google}
+          >
+            <Marker
+              onClick={this.onMarkerClick}
+              name={"Kenyatta International Convention Centre"}
+            />
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <AddEvent
-                eventCallback={this.setEvent}
-                data={this.state.events}
-              />
-            </div>
-            <div className="col-md-6">hello</div>
-          </div>
+
+            {marker}
+
+            <Marker
+              onClick={this.onMarkerClick}
+              name={this.state.markers[0].info.name}
+              position={this.state.markers[0].position}
+              icon={image}
+            />
+
+    
+
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}
+              onClose={this.onClose}
+            >
+              <div>
+                <h4>{this.state.selectedPlace.name}</h4>
+              </div>
+            </InfoWindow>
+          </CurrentLocation>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAhdbb0EH5BJper6b0NdYZQ2vRkjElScWA"
+})(App);
